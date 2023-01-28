234d574bde6d995aaa5b601b794be3237b552a0f
Prakash R <prakashramanukolanu1998@gmail.com>
2023-01-28 21:43:23 +0530

    Add a plot for dummy data using plotly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 
@@ -7,3 +8,7 @@ days = st.slider(label="Forecast days", min_value=1, max_value=5, help="please s
 data_required = st.selectbox(label="Select type of data to view", options=('Temperature', 'Sky Condition'))
 
 st.subheader(f"{data_required} for the next {days} days in {place}")
+
+
+figure = px.line(x=[1, 2, 3, 4, 5], y=[27, 29, 26, 24, 30], labels={'x': "Days", 'y': "Temperature (C)"})
+st.plotly_chart(figure)
