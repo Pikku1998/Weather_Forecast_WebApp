c2c7bce76b241bd9719ab6aa5912884b7ecad330
Prakash R <prakashramanukolanu1998@gmail.com>
2023-01-30 12:29:22 +0530

    Integrate frontend with backend Plot Temperature and show Sky Conditions

--- backend.py
+++ backend.py
@@ -4,12 +4,8 @@ API_KEY = 'f87e88970ee6293f9065b93092cf37aa'
 
 
 def get_data(place, forecast_days):
-    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     data = response.json()
-    filter_data = data['list'][:8 * forecast_days]
-    return filter_data
-
-
-if __name__ == "__main__":
-    print(len(get_data('tirana', 2)))
+    filtered_data = data['list'][:8 * forecast_days]
+    return filtered_data
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 
 place = st.text_input("Place:")
 days = st.slider(label="Forecast days", min_value=1, max_value=5, help="please select the number of days")
-data_required = st.selectbox(label="Select type of data to view", options=('Temperature', 'Sky Condition'))
+data_required = st.selectbox(label="Select type of data to view", options=('Temperature', 'Sky Conditions'))
 
-st.subheader(f"{data_required} for the next {days} days in {place}")
+if place:
+    data = get_data(place, days)
+    st.subheader(f"{data_required} for the next {days} days in {place.title()}")
 
+    if data_required == 'Temperature':
+        x_axis_days = [item['dt_txt'] for item in data]
+        y_axis_temp = [each['main']['temp'] for each in data]
+        figure = px.line(x=x_axis_days, y=y_axis_temp, labels={'x': "Days", 'y': "Temperature (C)"})
+        st.plotly_chart(figure)
 
-figure = px.line(x=[1, 2, 3, 4, 5], y=[27, 29, 26, 24, 30], labels={'x': "Days", 'y': "Temperature (C)"})
-st.plotly_chart(figure)
+    if data_required == 'Sky Conditions':
+        sky_conditions = [item['weather'][0]['main'] for item in data]
+        image_dict = {'Clear': 'images/clear.png',
+                      'Clouds': 'images/cloud.png',
+                      'Rain': 'images/rain.png',
+                      'Snow': 'images/snow.png'}
+        sky_images = [image_dict[item] for item in sky_conditions]
+        st.image(sky_images, width=150)
