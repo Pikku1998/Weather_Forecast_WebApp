220239c6b983cb9bcb9c5c814c1a5ca8e90934c6
Prakash R <prakashramanukolanu1998@gmail.com>
2023-01-30 12:55:54 +0530

    Add captions to image Fix the error when city not found.

--- backend.py
+++ backend.py
@@ -7,5 +7,8 @@ def get_data(place, forecast_days):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     data = response.json()
-    filtered_data = data['list'][:8 * forecast_days]
-    return filtered_data
+    if data['message'] == 'city not found':
+        return 'city not found'
+    else:
+        filtered_data = data['list'][:8 * forecast_days]
+        return filtered_data
--- main.py
+++ main.py
@@ -4,25 +4,31 @@ from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 
-place = st.text_input("Place:")
+place = st.text_input("Place:", placeholder='Please enter a city name')
 days = st.slider(label="Forecast days", min_value=1, max_value=5, help="please select the number of days")
 data_required = st.selectbox(label="Select type of data to view", options=('Temperature', 'Sky Conditions'))
 
 if place:
     data = get_data(place, days)
-    st.subheader(f"{data_required} for the next {days} days in {place.title()}")
 
-    if data_required == 'Temperature':
-        x_axis_days = [item['dt_txt'] for item in data]
-        y_axis_temp = [each['main']['temp'] for each in data]
-        figure = px.line(x=x_axis_days, y=y_axis_temp, labels={'x': "Days", 'y': "Temperature (C)"})
-        st.plotly_chart(figure)
+    if data != 'city not found':
+        st.subheader(f"{data_required} for the next {days} days in {place.title()}")
 
-    if data_required == 'Sky Conditions':
-        sky_conditions = [item['weather'][0]['main'] for item in data]
-        image_dict = {'Clear': 'images/clear.png',
-                      'Clouds': 'images/cloud.png',
-                      'Rain': 'images/rain.png',
-                      'Snow': 'images/snow.png'}
-        sky_images = [image_dict[item] for item in sky_conditions]
-        st.image(sky_images, width=150)
+        if data_required == 'Temperature':
+            x_axis_days = [item['dt_txt'] for item in data]
+            y_axis_temp = [each['main']['temp'] for each in data]
+            figure = px.line(x=x_axis_days, y=y_axis_temp, labels={'x': "Days", 'y': "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if data_required == 'Sky Conditions':
+            sky_conditions = [item['weather'][0]['main'] for item in data]
+            image_dict = {'Clear': 'images/clear.png',
+                          'Clouds': 'images/cloud.png',
+                          'Rain': 'images/rain.png',
+                          'Snow': 'images/snow.png'}
+            sky_images = [image_dict[item] for item in sky_conditions]
+            image_captions = [item['dt_txt'] for item in data]
+            st.image(sky_images, caption=image_captions, width=150)
+
+    else:
+        st.subheader("City not found, Please enter a valid city name.")
